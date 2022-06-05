6e2b6d357f74d4f91119f4162d67f3d9c48b7726
Shane Orr <shane.j.orr@gmail.com>
2022-06-04 22:39:55 -0400

    start working on teacher survey

---
+++ notes/scrath_pad.R
@@ -0,0 +1,10 @@
+# This file represents scrath work. Don't assume anything in this file actually works.
+# It is not included in the package build.
+
+library(dplyr)
+library(tidyr)
+library(googlesheets4)
+
+# cleaning teacher survey ---------------
+# fresno
+teacher_survey_pre_url <- "https://docs.google.com/spreadsheets/d/1NP1oE0wa4nl2e7sV3ETcPvCIpnPMlNgbx1SImeAZ-0o/edit?resourcekey#gid=35865763"
